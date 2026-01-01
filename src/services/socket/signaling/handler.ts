605546677cfc6191c758ebad81d3be2134245de8
Enlil <116581871+Abolfazl2049@users.noreply.github.com>
2026-01-01 19:49:39 +0330

    signaling

--- src/services/socket/signaling/handler.ts
+++ src/services/socket/signaling/handler.ts
@@ -1,16 +1,24 @@
 import {Server} from "socket.io";
 import {User} from "../../user/entities.js";
 import {MySocket} from "../types.js";
+import {Enclave} from "#src/services/enclave/entities.js";
 
 export const registerSignalingNamespace = (io: Server) => {
   const signaling = io.of("/signaling");
+  const userSockets = new Map<string, MySocket>();
 
   signaling.on("connection", async (socket: MySocket) => {
     const userId = socket.handshake.auth.userId;
+    const enclaveId = socket.handshake.auth.enclaveId;
     try {
       const user = await User.findByPk(userId);
-      if (user) {
+      const enclave = await Enclave.findByPk(enclaveId);
+      if (user && enclave) {
         socket.data.user = user;
+        socket.data.enclave = enclave;
+        socket.join(enclaveId);
+        userSockets.set(userId, socket);
+
         console.log("User connected to signaling:", socket.id, "userId:", userId);
       } else {
         socket.disconnect();
@@ -20,38 +28,60 @@ export const registerSignalingNamespace = (io: Server) => {
       socket.disconnect();
     }
 
+    // Join
+    socket.on("join", data => {
+      socket.to(enclaveId).emit("join", {
+        userId: socket.data?.user.id,
+        enclaveId: enclaveId
+      });
+    });
+
     // WebRTC Offer
     socket.on("offer", data => {
-      console.log("Offer received from:", socket.data?.user.id);
-      // Forward to the other peer
-      socket.to(data.targetSocketId).emit("offer", {
-        from: socket.id,
-        userId: socket.data?.user.id,
-        offer: data.offer
-      });
+      const targetSocket = userSockets.get(data.targetUserId);
+      if (targetSocket) {
+        targetSocket.emit("offer", {
+          from: socket.id,
+          userId: socket.data?.user.id,
+          offer: data.offer,
+          enclaveId: enclaveId
+        });
+      }
     });
 
     // WebRTC Answer
     socket.on("answer", data => {
-      console.log("Answer received from:", socket.data?.user.id);
-      socket.to(data.targetSocketId).emit("answer", {
-        from: socket.id,
-        userId: socket.data?.user.id,
-        answer: data.answer
-      });
+      const targetSocket = userSockets.get(data.targetUserId);
+      if (targetSocket) {
+        targetSocket.emit("answer", {
+          from: socket.id,
+          userId: socket.data?.user.id,
+          answer: data.answer,
+          enclaveId: enclaveId
+        });
+      }
     });
 
     // ICE Candidate
-    socket.on("ice-candidate", data => {
-      socket.to(data.targetSocketId).emit("ice-candidate", {
-        from: socket.id,
-        userId: socket.data?.user.id,
-        candidate: data.candidate
-      });
+    socket.on("candidate", data => {
+      const targetSocket = userSockets.get(data.targetUserId);
+      if (targetSocket) {
+        targetSocket.emit("candidate", {
+          from: socket.id,
+          userId: socket.data?.user.id,
+          candidate: data.candidate,
+          enclaveId: enclaveId
+        });
+      }
     });
 
     socket.on("disconnect", () => {
       console.log("User disconnected from signaling:", socket.id, "userId:", socket.data?.user.id);
+      socket.to(enclaveId).emit("left", {
+        userId: socket.data?.user.id,
+        enclaveId: enclaveId
+      });
+      userSockets.delete(userId);
     });
   });
 };
--- src/services/socket/types.d.ts
+++ src/services/socket/types.d.ts
@@ -1,5 +1,6 @@
 import {Socket} from "socket.io";
 import {User} from "../user/entities.ts";
+import {Enclave} from "../enclave/entities.ts";
 
 interface MySocket extends Socket {
   data: SocketData;
@@ -7,5 +8,6 @@ interface MySocket extends Socket {
 interface SocketData {
   user: User;
   status: "searching" | "processing";
+  enclave: Enclave;
 }
 export {MySocket, SocketData};
